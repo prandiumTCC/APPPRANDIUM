ae424fc98fcfa7d74442b2a8ccd0dd4cdeb4b70a
PRANDIUM <tccprandium@gmail.com>
2019-06-19 02:39:20 -0300

    adicionar graficos e lib

--- src/screen/telas/Grafico.js
+++ src/screen/telas/Grafico.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-// import { LineChart } from "react-native-chart-kit";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { LineChart, ProgressChart, PieChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
 
 class Grafico extends Component {
   static navigationOptions = {
@@ -17,25 +18,118 @@ class Grafico extends Component {
   };
 
   render() {
-    // const data = {
-    //   labels: ["January", "February", "March", "April", "May", "June"],
-    //   datasets: [
-    //     {
-    //       data: [20, 45, 28, 80, 99, 43],
-    //       color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})` // optional
-    //     }
-    //   ]
-    // };
+    const chartConfig = {
+      backgroundGradientFrom: "#1E2923",
+      backgroundGradientTo: "#08130D",
+      color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+      strokeWidth: 2 // optional, default 3
+    };
+    const data3 = [
+      {
+        name: "Seoul",
+        population: 21500000,
+        color: "rgba(131, 167, 234, 1)",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+      {
+        name: "Toronto",
+        population: 2800000,
+        color: "#F00",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+      {
+        name: "Beijing",
+        population: 527612,
+        color: "red",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+      {
+        name: "New York",
+        population: 8538000,
+        color: "#ffffff",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+      {
+        name: "Moscow",
+        population: 11920000,
+        color: "rgb(0, 0, 255)",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      }
+    ];
+    const data1 = [0.4, 0.6, 0.8];
+    const data = {
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [20, 45, 28, 80, 99, 43],
+          color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})` // optional
+        }
+      ]
+    };
     return (
-      <View style={styles.fundo}>
-        <Text style={styles.titleGhart}>Geral</Text>
-        {/* <LineChart
-          data={data}
-          width={screenWidth}
-          height={220}
-          chartConfig={chartConfig}
-        /> */}
-      </View>
+      <ScrollView style={styles.fundo}>
+        <View>
+          <Text style={styles.titleGhart}>Geral</Text>
+          <LineChart
+            data={{
+              labels: ["January", "February", "March", "April", "May", "June"],
+              datasets: [
+                {
+                  data: [
+                    Math.random() * 200,
+                    Math.random() * 100,
+                    Math.random() * 50,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100
+                  ]
+                }
+              ]
+            }}
+            width={330} // from react-native
+            height={220}
+            yAxisLabel={"$"}
+            chartConfig={{
+              backgroundColor: "#e26a00",
+              backgroundGradientFrom: "#fb8c00",
+              backgroundGradientTo: "#ffa726",
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
+          <Text style={styles.titleGhart}>Geral</Text>
+          <ProgressChart
+            data={data1}
+            width={330}
+            height={220}
+            chartConfig={chartConfig}
+          />
+          <Text style={styles.titleGhart}>Geral</Text>
+          <PieChart
+            data={data3}
+            width={330}
+            height={220}
+            chartConfig={chartConfig}
+            accessor="population"
+            backgroundColor="transparent"
+            paddingLeft="15"
+            absolute
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
